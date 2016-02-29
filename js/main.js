62344e1bd0126f0b72aa1c7a564e0cca52c7a1c1
Matthew Litwin <mlitwin@kcptech.com>
2016-02-28 21:41:52 -0800

    cleanup

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@
  *
 */
 
-
 (function($) {
 	$("#cycleReset").click(function() {
 		$(".cycler").each(function() {
@@ -12,16 +11,23 @@
 		});		
 	});
 
+	// tortice and hare just run together - testing the animation code.
+	function runrunrunaround(model) {
+		var anim = [];
+		var i, cur = 0;
+		for(i = 0; i <= model.maxIndex; i++) {
+			anim.push({tortice: cur, hare: cur});
+			cur = Cycler.nextElement(cur, model);
+		}
+
+		return anim;	
+	}
+
 	$("#cycleGo").click(function() {
 		$(".cycler").each(function() {
 			var $self = $(this);
 			var model = $self.data("cycleData");
-			var anim = [];
-			var i, cur = 0;
-			for(i = 0; i <= model.maxIndex; i++) {
-				anim.push({tortice: cur, hare: cur});
-				cur = Cycler.nextElement(cur, model);
-			}
+			var anim = runrunrunaround(model);
 
 			Cycler.animate($self, anim);
 
